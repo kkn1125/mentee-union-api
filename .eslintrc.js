6dd33b43c9b86a77d56b4437b2de340018ca1746
kkn1125 <xpfjrnseks1@naver.com>
2023-11-23 02:56:05 +0900

    🛠️ fix: eslint rules 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,5 +29,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 };
