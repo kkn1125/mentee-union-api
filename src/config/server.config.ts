ad20f8614b0402600b3a56487992301a379f7225
kkn1125 <xpfjrnseks1@naver.com>
2023-11-22 18:32:52 +0900

    🛠️ fix: mailer 환경변수 등록

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -3,4 +3,5 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('server', () => ({
   host: process.env.HOST,
   port: process.env.PORT || 8080,
+  dsn: process.env.SENTRY_DSN,
 }));
