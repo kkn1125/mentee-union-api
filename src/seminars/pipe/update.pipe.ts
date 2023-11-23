851792587d4e7b25ef98d5796ea66af92a889e0a
kkn1125 <xpfjrnseks1@naver.com>
2023-11-23 14:49:49 +0900

    🛠️ fix: transform에서 request의 params를 가져오기 위해 inject로 request 받아와 아이디 전달 처리

--- src/seminars/pipe/update.pipe.ts
+++ src/seminars/pipe/update.pipe.ts
@@ -1,26 +1,41 @@
 import { ApiResponseService } from '@/api-response/api-response.service';
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  Inject,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
 import { SeminarsService } from '../seminars.service';
+import { Request } from 'express';
+import { REQUEST } from '@nestjs/core';
 
 @Injectable()
 export class UpdatePipe implements PipeTransform {
-  constructor(private readonly seminarsService?: SeminarsService) {}
+  constructor(
+    @Inject(REQUEST)
+    private readonly request: Request,
+    private readonly seminarsService?: SeminarsService,
+  ) {}
 
   async transform(value: any, metadata: ArgumentMetadata) {
-    console.log(metadata);
     if (metadata.type !== 'body') return value;
 
-    const existingData = await this.seminarsService.findOne(value.id);
-    if (!existingData) ApiResponseService.NOT_FOUND('not found seminar');
+    try {
+      const existingData = await this.seminarsService
+        .getRepository()
+        .findOneOrFail({ where: { id: +this.request.params.id } });
 
-    console.log({
-      ...existingData,
-      ...value,
-    });
+      console.log({
+        ...existingData,
+        ...value,
+      });
 
-    return {
-      ...existingData,
-      ...value,
-    };
+      return {
+        ...existingData,
+        ...value,
+      };
+    } catch (error) {
+      ApiResponseService.NOT_FOUND(error, 'not found seminar');
+    }
   }
 }
