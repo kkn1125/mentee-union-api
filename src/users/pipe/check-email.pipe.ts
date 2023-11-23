44d74d1d3624b2edc65e339eb789e78532f0fb29
kkn1125 <xpfjrnseks1@naver.com>
2023-11-23 16:36:53 +0900

    🛠️ fix: check email pipe에서 email format 확인하는 정규식 작성, 자세한 내용은 주석 참조

--- src/users/pipe/check-email.pipe.ts
+++ src/users/pipe/check-email.pipe.ts
@@ -1,3 +1,4 @@
+import { ApiResponseService } from '@/api-response/api-response.service';
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 
 @Injectable()
@@ -5,9 +6,23 @@ export class CheckEmailPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
     if (metadata.type !== 'body') return value;
 
-    console.log('body check', metadata.data, value);
-    // if(value.email.match())
+    // console.log('body check', metadata.data, value);
+    /* email format regex */
+    /**
+     * 규칙은 다음과 같습니다.
+     * 1. 사용자명은 2자 이상, 영문부터 시작
+     * 2. 도메인은 2자 이상, 영문부터 시작
+     * 3. 최상위 도메인은 2자 이상, 영문만 취급
+     * 이메일 구성에 관한 문건 @see https://namu.wiki/w/%EC%9D%B4%EB%A9%94%EC%9D%BC#s-3.1
+     */
+    const isEmailFormat = value.match(
+      /\b(?![0-9_\-]+)(?=.*[A-Za-z])(?=.*[_\-0-9]*)[A-Za-z_\-0-9]{2,}\@(?![0-9_\-]+)[ㄱ-힣A-Za-z0-9_\-]{2,}\.(?=.*[A-Za-z])[A-Za-z]{2,}\b/g,
+    );
 
-    return value;
+    if (isEmailFormat) {
+      return value;
+    } else {
+      ApiResponseService.BAD_REQUEST('not matched email format');
+    }
   }
 }
