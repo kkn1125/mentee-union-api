907c440a3747f2cc61db2723b55d7a6091430054
kkn1125 <xpfjrnseks1@naver.com>
2023-12-06 22:41:06 +0900

    ✨ feat: 채팅 채널 매칭 시스템 제작

--- src/channel/dto/update-channel.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateChannelDto } from './create-channel.dto';
-
-export class UpdateChannelDto extends PartialType(CreateChannelDto) {
-  id: number;
-}
