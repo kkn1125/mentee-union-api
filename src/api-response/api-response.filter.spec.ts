177dca42fef542d9dfcfcc20b7c8444f7c2c878e
kkn1125 <xpfjrnseks1@naver.com>
2024-01-04 13:54:24 +0900

    ❌ del: 불필요한 테스트 파일 제거, 각 파일은 임시파일 prefix 붙여서 모두 수정, 각 도메인에 관한 테스트 파일은 소정 진행

--- src/api-response/api-response.filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ApiResponseFilter } from './api-response.filter';
-
-describe('ApiResponseFilter', () => {
-  it('should be defined', () => {
-    expect(new ApiResponseFilter()).toBeDefined();
-  });
-});
--- src/api-response/api-response.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ApiResponseInterceptor } from './api-response.interceptor';
-
-describe('ApiResponseInterceptor', () => {
-  it('should be defined', () => {
-    expect(new ApiResponseInterceptor()).toBeDefined();
-  });
-});
